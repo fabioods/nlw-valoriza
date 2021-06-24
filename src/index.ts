4c50fe996518021e0a15256af04f1acab209d3ef
Fábio dos Santos <fah_ds@live.com>
2021-06-24 19:42:43 -0300

    feat: add middleware para interceptar erros

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import './database';
 import { router } from './routes';
 
@@ -7,6 +8,16 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+app.use(
+  (err: Error, req: Request, res: Response, next: NextFunction): Response => {
+    if (err instanceof Error) {
+      return res.status(400).json({ error: err.message });
+    }
+    return res
+      .status(500)
+      .json({ status: 'error', message: 'Internal server error' });
+  },
+);
 
 app.listen(5000, () => {
   console.log("🚀 let's start");

4a65796aeb45143a111277a5d265b411cdae1fde
Fábio Santos <fabio.santos@fundacaoabc.org>
2021-07-21 19:34:04 -0300

    refactor: alterado no cadastro de elogios para o user_sender ser sempre o usuário logado

--- src/controllers/CreateComplimentController.ts
+++ src/controllers/CreateComplimentController.ts
@@ -3,13 +3,14 @@ import { CreateComplimentService } from '../services/CreateComplimentService';
 
 export class CreateComplimentController {
   async handle(req: Request, res: Response): Promise<Response> {
-    const { message, tag_id, user_receiver, user_sender } = req.body;
+    const { message, tag_id, user_receiver } = req.body;
+    const { user_id } = req;
     const createComplimentService = new CreateComplimentService();
     const compliment = await createComplimentService.execute({
       message,
       tag_id,
       user_receiver,
-      user_sender,
+      user_sender: user_id,
     });
     return res.json(compliment);
   }

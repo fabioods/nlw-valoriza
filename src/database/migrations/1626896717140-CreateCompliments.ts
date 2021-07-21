cbdb73e33e73e0e0325f44a820f4e480f62ac7b0
Fábio Santos <fabio.santos@fundacaoabc.org>
2021-07-21 17:05:23 -0300

    feat: criada migration para criação da tabela de compliments

---
+++ src/database/migrations/1626896717140-CreateCompliments.ts
@@ -0,0 +1,90 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreateCompliments1626896717140 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'compliments',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isNullable: false,
+            isPrimary: true,
+          },
+          {
+            name: 'user_sender',
+            type: 'uuid',
+            isNullable: false,
+          },
+          {
+            name: 'user_receiver',
+            type: 'uuid',
+            isNullable: false,
+          },
+          {
+            name: 'tag',
+            type: 'uuid',
+            isNullable: false,
+          },
+          {
+            name: 'message',
+            type: 'text',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            isNullable: false,
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            isNullable: false,
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKeys('compliments', [
+      new TableForeignKey({
+        columnNames: ['user_sender'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        name: 'fk_compliments_user_sender',
+      }),
+      new TableForeignKey({
+        columnNames: ['user_receiver'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        name: 'fk_compliments_user_receiver',
+      }),
+      new TableForeignKey({
+        columnNames: ['tag'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'tags',
+        name: 'fk_compliments_tag',
+      }),
+    ]);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey(
+      'compliments',
+      'fk_compliments_user_sender',
+    );
+    await queryRunner.dropForeignKey(
+      'compliments',
+      'fk_compliments_user_receiver',
+    );
+    await queryRunner.dropForeignKey('compliments', 'fk_compliments_tag');
+    await queryRunner.dropTable('compliments');
+  }
+}

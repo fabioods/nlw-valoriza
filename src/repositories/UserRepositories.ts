917f8bb9b8a783609ac9fa3a305122f3e464e013
Fábio Santos <fabio.santos@fundacaoabc.org>
2021-07-21 16:22:14 -0300

    feat: criado metodo para buscar usuario por Email no UserRepository

--- src/repositories/UserRepositories.ts
+++ src/repositories/UserRepositories.ts
@@ -2,4 +2,9 @@ import { EntityRepository, Repository } from 'typeorm';
 import { User } from '../entities/User';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async getUserByEmail(email: string): Promise<User> {
+    const user = await this.findOne({ where: { email } });
+    return user;
+  }
+}

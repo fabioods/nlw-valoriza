22ce00e71dc229c1c093087c3861c5d44245e856
Fábio Santos <fabio.santos@fundacaoabc.org>
2021-07-21 12:03:15 -0300

    feat: adicionado TagsRepositories

---
+++ src/repositories/TagsRepositories.ts
@@ -0,0 +1,10 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { Tag } from '../entities/Tag';
+
+@EntityRepository(Tag)
+export class TagRepository extends Repository<Tag> {
+  async getByName(name: string): Promise<Tag> {
+    const tag = await this.findOne({ where: { name } });
+    return tag;
+  }
+}

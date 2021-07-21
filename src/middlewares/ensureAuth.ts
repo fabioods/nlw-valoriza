98309853fc5b4c9cf9b385eb5e677e38b28ca83b
Fábio Santos <fabio.santos@fundacaoabc.org>
2021-07-21 19:26:36 -0300

    feat: middlewara para verificar se o usuário está autenticado no sistema via token

--- src/middlewares/ensureAuth.ts
+++ src/middlewares/ensureAuth.ts
@@ -1,13 +1,33 @@
 import { NextFunction, Request, Response } from 'express';
+import { verify, decode } from 'jsonwebtoken';
+import { NoExistsError, InvalidParameterError } from '../utils/error';
+
+interface TokenOptions {
+  email: string;
+  name: string;
+  iat: number;
+  exp: number;
+  sub: string;
+}
 
 export function ensureAuth(
   req: Request,
   res: Response,
   next: NextFunction,
 ): Response | void {
-  const admin = true;
+  const auth = req.headers.authorization;
+  if (!auth) throw new NoExistsError('token');
 
-  if (admin) return next();
+  const [, token] = auth.split(' ');
 
-  return res.status(401).json({ message: 'User not authenticated' });
+  const verifyToken = verify(token, '7094f94201abca95b79864f60c03562b');
+  if (!verifyToken) throw new InvalidParameterError('token');
+
+  const { email, name, sub } = decode(token) as TokenOptions;
+
+  req.email = email;
+  req.name = name;
+  req.user_id = sub;
+
+  return next();
 }

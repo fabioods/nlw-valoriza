29155ef27f190a867177728f5d0fb73f548e6cfa
Fábio Santos <fabio.santos@fundacaoabc.org>
2021-07-22 13:11:47 -0300

    feat: adicionadas rotas para consultar elogios enviados e recebidos pelo usuário logado

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,8 @@ import {
   AuthenticateUserController,
   CreateComplimentController,
 } from './controllers';
+import { ListComplimentsByUserReceiverController } from './controllers/ListComplimentsByUserReceiverController';
+import { ListComplimentsByUserSenderController } from './controllers/ListComplimentsByUserSenderController';
 import { ensureAuth, ensureAdmin } from './middlewares';
 
 const router = Router();
@@ -13,10 +15,24 @@ const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authUserController = new AuthenticateUserController();
 const createComplimentController = new CreateComplimentController();
+const listComplimentsByUserSenderController =
+  new ListComplimentsByUserSenderController();
+const listComplimentsByUserReceiverController =
+  new ListComplimentsByUserReceiverController();
 
 router.post('/auth', authUserController.handle);
 router.post('/users', createUserController.handle);
 router.post('/tags', ensureAuth, ensureAdmin, createTagController.handle);
 router.post('/compliments', ensureAuth, createComplimentController.handle);
+router.get(
+  '/users/compliments/send',
+  ensureAuth,
+  listComplimentsByUserSenderController.handle,
+);
+router.get(
+  '/users/compliments/receive',
+  ensureAuth,
+  listComplimentsByUserReceiverController.handle,
+);
 
 export { router };

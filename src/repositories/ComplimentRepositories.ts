9f3df135926de247ba1c17589ec45f113c9e8bb6
Fábio Santos <fabio.santos@fundacaoabc.org>
2021-07-22 13:16:45 -0300

    feat: adicionada os objetos de userSender, userReceiver e tag ao retorno das consultas de elogios do usuário

--- src/repositories/ComplimentRepositories.ts
+++ src/repositories/ComplimentRepositories.ts
@@ -4,12 +4,18 @@ import { Compliment } from '../entities/Compliment';
 @EntityRepository(Compliment)
 export class ComplimentRepository extends Repository<Compliment> {
   async getComplimentByUserSender(id: string): Promise<Compliment[]> {
-    const compliments = await this.find({ where: { user_sender: id } });
+    const compliments = await this.find({
+      where: { user_sender: id },
+      relations: ['userSender', 'userReceiver', 'tag'],
+    });
     return compliments;
   }
 
   async getComplimentByUserReceiver(id: string): Promise<Compliment[]> {
-    const compliments = await this.find({ where: { user_receiver: id } });
+    const compliments = await this.find({
+      where: { user_receiver: id },
+      relations: ['userSender', 'userReceiver', 'tag'],
+    });
     return compliments;
   }
 }

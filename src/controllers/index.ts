5b72f44ae04fa89b3dfe6dc6647d2bbf000183e8
Fábio Santos <fabio.santos@fundacaoabc.org>
2021-07-22 13:54:46 -0300

    feat: criado controller para listar tags

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,10 +2,16 @@ import { CreateUserController } from './CreateUserController';
 import { CreateTagController } from './CreateTagController';
 import { AuthenticateUserController } from './AuthenticateUserController';
 import { CreateComplimentController } from './CreateComplimentController';
+import { ListComplimentsByUserReceiverController } from './ListComplimentsByUserReceiverController';
+import { ListComplimentsByUserSenderController } from './ListComplimentsByUserSenderController';
+import { ListTagsController } from './ListTagsController';
 
 export {
   CreateUserController,
   CreateTagController,
   AuthenticateUserController,
   CreateComplimentController,
+  ListComplimentsByUserSenderController,
+  ListTagsController,
+  ListComplimentsByUserReceiverController,
 };
